f910a55220e4c131906c9db1227dbb2cfe9c8d07
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-10-26 23:05:28 -0400

    Add navigation drawer

--- app/src/main/java/com/example/android/moviesapp/MovieGridFragment.java
+++ app/src/main/java/com/example/android/moviesapp/MovieGridFragment.java
@@ -7,12 +7,18 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -26,17 +32,32 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MovieGridFragment extends Fragment {
 
     private MovieAdapter adapter;
     private ArrayList<Movie> movieList;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ArrayAdapter<String> drawerAdapter;
+    private ActionBarDrawerToggle drawerToggle;
+
+
+    /*since java has no map literals and searchCategories is a class variable, initialization
+      should be done in a static initializer */
+    private static final HashMap<String, String> searchCategories = new HashMap<>();
+    static {
+        searchCategories.put("Popular", "movie/popular");
+        searchCategories.put("Top Rated", "top_rated");
+    }
 
     public MovieGridFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -59,8 +80,63 @@ public class MovieGridFragment extends Fragment {
             }
         });
 
+        drawerLayout = (DrawerLayout) rootView.findViewById(R.id.drawer_layout);
+        drawerList = (ListView) rootView.findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        drawerAdapter = new ArrayAdapter<String>(getContext(),
+                R.layout.drawer_list_item, R.id.drawer_item, new ArrayList<String>(searchCategories.keySet()));
+        drawerList.setAdapter(drawerAdapter);
+        // Set the list's click listener
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        //change drawer icon
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_drawer);
+
+        setupDrawer();
+
         checkNetworkConnection();
         return rootView;
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // Highlight the selected item, update the title, and close the drawer
+            drawerList.setItemChecked(position, true);
+            //getActivity().setTitle(searchCategories.get(drawerAdapter.getItem(position)));
+            getActivity().setTitle(drawerAdapter.getItem(position));
+            drawerLayout.closeDrawer(drawerList);
+        }
+    }
+
+    private void setupDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActivity().invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActivity().invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(drawerToggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Activate the navigation drawer toggle
+        if (drawerToggle.onOptionsItemSelected(item)) return true;
+        return super.onOptionsItemSelected(item);
     }
 
     private void checkNetworkConnection() {
@@ -106,7 +182,6 @@ public class MovieGridFragment extends Fragment {
                 movie.setImageAddress(movieObject.getString(IMAGE_ADDRESS));
 
                 movieList.add(i, movie);
-
                 Log.v(LOG_TAG, movie.toString());
             }
             return movieList;
