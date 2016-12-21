3d4c781aba328e0a2f9bf32712ecbaec3256f62a
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-12-21 18:24:20 -0500

    Create database contract class and define column names for the favorite movies table

---
+++ app/src/main/java/com/example/android/moviesapp/database/MovieContract.java
@@ -0,0 +1,21 @@
+package com.example.android.moviesapp.database;
+
+import android.provider.BaseColumns;
+
+public final class MovieContract {
+
+    //the constructor is private to prevent the contract from being accidentally instantiated
+    private MovieContract(){}
+
+    /* Inner class that defines the table contents */
+    public static class FavoriteMovieEntry implements BaseColumns {
+        public static final String TABLE_NAME = "favorite_movies";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_RATING = "rating";
+        public static final String COLUMN_NAME_GENRE = "genre";
+        public static final String COLUMN_NAME_RELEASE = "release_date";
+        public static final String COLUMN_NAME_COUNTRY = "country";
+        public static final String COLUMN_NAME_OVERVIEW = "overview";
+        public static final String COLUMN_NAME_IMAGE = "image";
+    }
+}
