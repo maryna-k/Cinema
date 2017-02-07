49e92cf499975e897127840ca717d10bcb9b2a4b
Maryna Kalachova <kalachova.maryna@gmail.com>
2017-02-07 12:27:57 -0500

    Set background image to movie items in MovieGridFragment and show movie title in case if movie has no poster

--- app/src/main/java/com/example/android/moviesapp/MovieAdapter.java
+++ app/src/main/java/com/example/android/moviesapp/MovieAdapter.java
@@ -2,53 +2,63 @@ package com.example.android.moviesapp;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
     private ArrayList<Movie> mList;
-    private final String LOG_TAG = MovieAdapter.class.getSimpleName() + "LOG";
-    private Context context;
+    private static final String LOG_TAG = MovieAdapter.class.getSimpleName() + "LOG";
+    private static Context context;
     private OnItemClickListener listener;
 
-    public interface OnItemClickListener{
+    public interface OnItemClickListener {
         void onItemClick(Movie movie);
     }
 
-    public static  class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageViewItem;
+        private TextView titleView;
 
-        public ViewHolder(View view){
+        public ViewHolder(View view) {
             super(view);
             imageViewItem = (ImageView) view.findViewById(R.id.grid_poster);
+            titleView = (TextView) view.findViewById(R.id.title_without_poster);
         }
 
-        public void bind(final Movie movie, final OnItemClickListener listener){
-            String address = "http://image.tmdb.org/t/p/w780/" + movie.getImageAddress();
-            Picasso.with(itemView.getContext()).load(address).into(imageViewItem);
-            itemView.setOnClickListener(new View.OnClickListener(){
+        public void bind(final Movie movie, final OnItemClickListener listener) {
+            String imageAddress = movie.getImageAddress();
+            Log.v(LOG_TAG, "Poster address: " + imageAddress);
+            String fullImageAddress = "http://image.tmdb.org/t/p/w780/" + movie.getImageAddress();
+            Picasso.with(itemView.getContext()).load(fullImageAddress).into(imageViewItem);
+            if(imageAddress.equals("null")){
+                titleView.setText(movie.getTitle());
+                titleView.setVisibility(View.VISIBLE);
+            } else titleView.setVisibility(View.GONE);
+            itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v){
+                public void onClick(View v) {
                     listener.onItemClick(movie);
                 }
             });
         }
     }
 
-    public MovieAdapter(ArrayList<Movie> mList, OnItemClickListener listener){
+    public MovieAdapter(ArrayList<Movie> mList, OnItemClickListener listener) {
         this.mList = mList;
         this.listener = listener;
     }
 
     @Override
-    public MovieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public MovieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         context = parent.getContext();
         View itemView = LayoutInflater.from(context)
                 .inflate(R.layout.grid_item_movie, parent, false);
@@ -56,12 +66,12 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position){
+    public void onBindViewHolder(ViewHolder holder, int position) {
         holder.bind(mList.get(position), listener);
     }
 
     @Override
-    public int getItemCount(){
+    public int getItemCount() {
         return mList.size();
     }
 
@@ -72,20 +82,12 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         notifyDataSetChanged();
     }
 
-    public void addData(ArrayList<Movie> data){
+    public void addData(ArrayList<Movie> data) {
         mList.addAll(data);
         /*for(int i = 0; i < mList.size(); i++){
             Log.v(LOG_TAG, i+ ". " + mList.get(i).getTitle());
         }*/
-        this.notifyItemRangeChanged(mList.size()+1, data.size());
+        this.notifyItemRangeChanged(mList.size() + 1, data.size());
         this.notifyDataSetChanged();
-    }
-
-    public ArrayList<Movie> getAdapterData(){
-        ArrayList<Movie> adapterList = new ArrayList<>(mList.size());
-        for(int i = 0; i < mList.size(); i++){
-            adapterList.add(i, new Movie(mList.get(i)));
-        }
-        return adapterList;
     }
 }
